cf4c3b974b8b4b20dfc28cc1d4752c12ad4295d0
Cépagrave <bouss4guet@gmail.com>
2023-02-17 18:22:07 +0100

    83,84

--- PE_0083.py
+++ PE_0083.py
@@ -28,23 +28,47 @@ def PE83(matrix):
 
 	while True:
 		tot,here,visited = ways.pop()
-		print(tot,here,len(visited))
 		for neighbor in neighbors[here]:
 			if neighbor in visited: continue
 			if neighbor == (H-1,W-1): return tot+matrix[neighbor]
 			ways = sorted_insert(ways,(tot+matrix[neighbor],neighbor,visited|{neighbor}))
-		print(len(ways))
 
+import heapq
+def dijkstra(graph,node):
+
+    distances={node:float('inf') for node in graph}
+    distances[node]=0
+    came_from={node:None for node in graph}
+    queue=[(0,node)]
+    
+    while queue:
+        current_distance,current_node=heapq.heappop(queue)
+        for next_node,weight in graph[current_node].items():
+            distance_temp=current_distance+weight
+            if distance_temp<distances[next_node]:
+                distances[next_node]=distance_temp
+                came_from[next_node]=current_node
+                heapq.heappush(queue,(distance_temp,next_node))
+                
+    return distances,came_from
+
+def build_graph(matrix):
+	H = len(matrix)
+	W = len(matrix[0])
+	g = {(n:=i*W+j):{m:matrix[y][x] for (m,cond,y,x) 
+				in [(n-1,n%W,i,j-1),(n+1,(n+1)%W,i,j+1),(n-W,n>=W,i-1,j),(n+W,n<(H-1)*W,i+1,j)] if cond} 
+				for i in range(H) for j in range(W)}
+	return g
 
 with open("PE_0083.txt",'r') as f:
 	matrix = [[*map(int,line.split(','))] for line in f.read().split('\n') if line]
 
-matrix = [
-[5, 1 , 1, 20,1, 1,1],
-[9, 9 , 1, 20,1,20,1],
-[3, 1 , 1, 20,1,20,1],
-[2 ,20,20, 20,1,20,1],
-[1, 1 , 1,  2,1,30,1]]
+# matrix = [
+# [5, 1 , 1, 20,1, 1,1],
+# [9, 9 , 1, 20,1,20,1],
+# [3, 1 , 1, 20,1,20,1],
+# [2 ,20,20, 20,1,20,1],
+# [1, 1 , 1,  2,1,30,1]]
 
 # matrix = [
 # [131,673,234,103,18],
@@ -54,5 +78,14 @@ matrix = [
 # [805,732,524,37,331]
 # ]
 
-r = PE83(matrix)
-print(r)
+H,W = len(matrix),len(matrix[0])
+print(H*W)
+G = build_graph(matrix)
+# print(G)
+
+d = dijkstra(G,0)
+print(d)
+print(d[0][6399]+matrix[0][0])
+# r = PE83(matrix)
+# print(r)
+
